8422a2d46fc37fa631447b8bdcdf3b5d29a948e9
AutumnVN <autumnvnchino@gmail.com>
2025-01-13 21:23:30 +0700

    reset cooldown on swap hand too

--- src/main/java/autumnvn/hitSwapNerf/listener/PlayerEventListener.java
+++ src/main/java/autumnvn/hitSwapNerf/listener/PlayerEventListener.java
@@ -3,6 +3,7 @@ package autumnvn.hitSwapNerf.listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemHeldEvent;
+import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 
 public class PlayerEventListener implements Listener {
 
@@ -11,5 +12,10 @@ public class PlayerEventListener implements Listener {
          if (event.getNewSlot() >= 0 && event.getNewSlot() < 9 && event.getNewSlot() != event.getPreviousSlot()) {
              event.getPlayer().resetCooldown();
          }
+    }
+
+    @EventHandler
+    public void onPlayerSwapHandItems(PlayerSwapHandItemsEvent event) {
+        event.getPlayer().resetCooldown();
     }
 }
